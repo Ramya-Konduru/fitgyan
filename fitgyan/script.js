c39c0b3706d2091f7e626412d4d052e08cf7a435
Ramya Konduru <ramyapatrudu26@gmail.com>
2022-03-15 18:31:27 +0530

    new commit

---
+++ fitgyan/script.js
@@ -0,0 +1,11 @@
+// $(function () {
+//     $(document).scroll(function () {
+//         var $nav = $(".navbar-fixed-top");
+//         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//     });
+// });
+
+// $(window).scroll(function () {
+
+//     $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+// });
